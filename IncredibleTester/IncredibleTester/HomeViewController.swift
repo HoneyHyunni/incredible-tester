7b4553ad0cd05c701bfd31153abefca1788efdb7
Sanghyun <dev.sanghyun@gmail.com>
2023-11-28 09:57:38 +0900

    [FEAT] Home ViewController 구현

---
+++ IncredibleTester/IncredibleTester/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  IncredibleTester
+//
+//  Created by Sanghyun on 11/28/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func selectView(_ sender: Any) {
+        print("select view")
+    }
+    
+}
