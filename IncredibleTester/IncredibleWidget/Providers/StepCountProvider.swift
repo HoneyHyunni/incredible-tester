75005695146574eec73c892a52795ca564e2e540
Sanghyun <dev.sanghyun@gmail.com>
2023-12-18 15:15:05 +0900

    [FEAT] Widget 업데이트 정책 수정 및 스냅샷 코드 구현

--- IncredibleTester/IncredibleWidget/Providers/StepCountProvider.swift
+++ IncredibleTester/IncredibleWidget/Providers/StepCountProvider.swift
@@ -8,25 +8,37 @@
 import WidgetKit
 
 struct StepCountProvider: TimelineProvider {
+    
+    private func getTemporaryStepCount(seed date: Date) -> Int {
+        let dateComponents = Calendar.current.dateComponents([.hour, .minute], from: date)
+        
+        return (dateComponents.hour ?? 0) + (dateComponents.minute ?? 0)
+    }
+    
     func placeholder(in context: Context) -> StepCountEntry {
         StepCountEntry(date: Date(), stepCount: 500)
     }
 
     func getSnapshot(in context: Context, completion: @escaping (StepCountEntry) -> ()) {
-        let entry = StepCountEntry(date: Date(), stepCount: 500)
-        completion(entry)
+        let date = Date()
+        var stepCountEntry: StepCountEntry
+        
+        if context.isPreview {
+            stepCountEntry = StepCountEntry(date: date, stepCount: 777)
+        } else {
+            stepCountEntry = StepCountEntry(date: date, stepCount: self.getTemporaryStepCount(seed: date))
+        }
+        
+        completion(stepCountEntry)
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<StepCountEntry>) -> ()) {
         let date = Date()
-        let policyDate = Calendar.current.date(byAdding: .minute, value: 10, to: date)
         
-        let dateComponents = Calendar.current.dateComponents([.hour, .month, .second], from: date)
-        let tempStepCount = (dateComponents.hour ?? 0) + (dateComponents.month ?? 0) + (dateComponents.second ?? 0)
-        
-        let stepCountEntry = StepCountEntry(date: date, stepCount: tempStepCount)
-        let timeline = Timeline(entries: [stepCountEntry], policy: .after(policyDate ?? date))
+        let stepCountEntry = StepCountEntry(date: date, stepCount: self.getTemporaryStepCount(seed: date))
+        let timeline = Timeline(entries: [stepCountEntry], policy: .atEnd)
         
         completion(timeline)
     }
+    
 }
